321d28588b79a7166fa3d3d42a491be37a8d643c
unknown <tirritoroberto@gmail.com>
2021-12-30 12:39:54 +0100

    codeweek raggiunta

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -2,50 +2,55 @@ import style from "./Body.module.scss";
 import Card from "../Card";
 import React from "react";
 
-const API= "https://jsonplaceholder.typicode.com/users";
+// const API= "https://jsonplaceholder.typicode.com/users";
 
-const getUtenti = async () => {
-    const response = await fetch(API);
-    return response.json();
-}
+// const getUtenti = async () => {
+//     const response = await fetch(API);
+//     return response.json();
+// }
 
-class Body extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: []
-        }
-    }
+// class Body extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             list: []
+//         }
+//     }
 
-    componentDidMount() {
-        /*const setListaUtenti = async () => {
-            const response = await fetch(API);
-            this.setState({
-                list
-            })
-        };*/
-        const setListaUtenti = () => {
-            fetch(API).then((response) => response.json())
-            .then((list) => {
-                this.setState({list})
-            })
-        }
-        setListaUtenti();
-    }
+//     componentDidMount() {
+//         /*const setListaUtenti = async () => {
+//             const response = await fetch(API);
+//             this.setState({
+//                 list
+//             })
+//         };*/
+//         const setListaUtenti = () => {
+//             fetch(API).then((response) => response.json())
+//             .then((list) => {
+//                 this.setState({list})
+//             })
+//         }
+//         setListaUtenti();
+//     }
 
-    render() {
-        const cards = [];
-        for(let i=0; this.state.list.length; i++ )  {
-            cards.push( <Card input={this.state.list[i]} uniqueId={i} /> )
-        }
+//     render() {
+//         const cards = [];
+//         for(let i=0; this.state.list.length; i++ )  {
+//             cards.push( <Card input={this.state.list[i]} uniqueId={i} /> )
+//         }
 
-        return (
-            <div id="cardContainer" className={style.user}>
-            {cards}
-        </div>
-        );
-    }
-}
+//         return (
+//             <div id="cardContainer" className={style.user}>
+//             {cards}
+//         </div>
+//         );
+//     }
+// }
 
+const Body = () => {
+    return(
+       <Card/>
+    )
+};
 
 export default Body
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,28 @@
 import style from "./Card.module.scss";
 
-const Card = (cardData) => {
-    const user = cardData.input;
-    const uniqueId = cardData.uniqueId;
-    return (
-        <div id={"card"+uniqueId}>
-            <h4>${user.name}</h4>
+// const Card = (cardData) => {
+//     const user = cardData.input;
+//     const uniqueId = cardData.uniqueId;
+//     return (
+//         <div id={"card"+uniqueId}>
+//             <h4>${user.name}</h4>
+//         </div>
+
+      
+//     )
+// };
+
+const Card = () => {
+    return(
+        <div className={style.card}>
+        <h2 id="name" className={style.name}>Leanne Graham</h2>
+        <h4 id="username" className={style.username}>Bret</h4>
+        <p id="email" className={style.email}><a><strong>e </strong></a>Sincere@april.biz</p>
+        <button id="address" className={style.address}><strong>address</strong></button>
+        <p id="phone" className={style.phone}><a><strong>ph </strong></a>1-770-736-8031 x56442</p>
+        <p id="website" className={style.website}><a><strong>w </strong></a>hildegard.org</p>
+        <button id="company" className={style.company}><strong>company</strong></button>
+
         </div>
     )
 };
